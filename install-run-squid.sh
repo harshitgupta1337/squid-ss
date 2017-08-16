1b3b95f2263a6e4bfeae445ad0f34cf4cfeb1448
Ubuntu <squid@squid-extension-test.3ouvyxhqv2leheawrpgphx34hf.bx.internal.cloudapp.net>
2017-08-16 17:57:01 +0000

    Removed kill commands

--- install-run-squid.sh
+++ install-run-squid.sh
@@ -1,17 +1,18 @@
 sudo apt install -y squid
+sudo apt install -y squid3
 
 sudo cp ./squid.conf /etc/squid/squid.conf
 
 sleep 3
 
-sudo killall squid
-sleep 1
-sudo killall squid
-sleep 1
-sudo killall squid3
-sleep 1
-sudo killall squid3
-sleep 1
+#sudo killall squid
+#sleep 1
+#sudo killall squid
+#sleep 1
+#sudo killall squid3
+#sleep 1
+#sudo killall squid3
+#sleep 1
 
 sudo squid3 -Nz
 
